7044d0641f876b51118dd56eff1c30a370d6d51e
Taylor Svendsen <taysvendsen@gmail.com>
2018-03-06 19:02:16 -0800

    adds hirb gem to give table view of objects on rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,27 @@ Customer.destroy_all
 Movie.destroy_all
 Rental.destroy_all
 
+genres = ["Horror", "Comedy", "Action", "Adventure", "Romance", "Thriller", "Drama", "Animation", "Science Fiction", "Documentary", "Musical", "Independent", "Fantasy", "Foreign", "Crime"]
 
-10.times do |i|
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password_hash: Faker::Internet.password(8), employee_id: Faker::Number.number(4))
+formats = ["VHS", "DVD", "Blu-ray"]
+
+years = *(1965..2018)
+
+user_ary = []
+customer_ary = []
+movie_ary = []
+
+10.times do
+  user_ary << User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password_hash: Faker::Internet.password(8), employee_id: Faker::Number.number(4))
+
 end
 
-50.times do |i|
-  Customer.create!(full_name: Faker::Name.name, address: Faker::Address.street_address, dl_number: Faker::Number.number(6))
+50.times do
+   customer_ary << Customer.create!(full_name: Faker::Name.name, address: Faker::Address.street_address, dl_number: Faker::Number.number(6))
 end
 
-100.times do |i|
-  Movie.create!(title: Faker::Lorem.sentence(0, true, 4), genre: "Horror", format: "VHS", status: true, year: 1985, rental_cost: 3, barcode: 123456)
+100.times do
+  movie_ary << Movie.create!(title: Faker::Lorem.sentence(0, true, 4), genre: genres.sample, format: formats.sample, status: true, year: years.sample, rental_cost: 3, barcode: 0001)
 end
 
-Rental.create!(user_id: 1, customer_id: 1, movie_id: 1, rental_date: Date.today)
+Rental.create!(user_id: user_ary.sample, customer_id: customer_ary.sample, movie_id: movie_ary.sample, rental_date: Date.today)
